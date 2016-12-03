63a75ed3257d904b5c7b263c6cf017b1acb5f42b
Anibal Martinez <anibalix@gmail.com>
2016-12-03 16:25:19 -0300

    Update web.php

--- OUTHLETES/Outhletes/routes/web.php
+++ OUTHLETES/Outhletes/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index','IndexController@index');
+Route::get('/signup','SignupController@signup');
+Route::get('/login','LoginController@login');
+Route::get('/faqs','FaqsController@faqs');
+Route::get('/profile','ProfileController@profile');
