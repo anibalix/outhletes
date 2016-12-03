8bfc5c58765f655f16a142a7c675ac270a2770e6
Anibal Martinez <anibalix@gmail.com>
2016-12-03 16:26:38 -0300

    Add files via upload

---
+++ OUTHLETES/Outhletes/resources/views/index.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>OUTHLETES Portal</title>
+    <link rel="stylesheet" href="css/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <header class="header">
+        <div class="btn_container">
+            <ul class="nav_access">
+                <li class="login_btn"><a href="login">LOG IN</a></li>
+                <li class="signup_btn"><a href="signup">SIGN UP</a></li>
+            </ul>
+        </div>
+    </header>
+    <!--- Main- ----->
+    <div class="main_container">
+        <!--section1 -->
+        <section id="section1"class="section1">
+            <div class="title">
+                <!-- <h1>OUTHLETES</h1> -->
+                <img src="imagenes/logoOuthletes.png" alt="" id="logo_full"/>
+                <h2>El deporte es mejor si es social</h2>
+            </div>
+        <div class="desktop_access">
+          <div class="btn_container">
+              <ul class="nav_access">
+                  <li class="login_btn"><a href="login">LOG IN</a></li>
+                  <li class="signup_btn"><a href="signup">SIGN UP</a></li>
+              </ul>
+          </div>
+        </div>
+        <div class="scrollDown1">
+            <a href="#scrollDown1">SCROLL</a>
+        </div>
+        </section>
+        <section id="scrollDown1" class="section2">
+            <div class="container_section2">
+                <div class="section2_a">
+                    <div class="section2_a_text">
+                      <h2>
+                      Motiva
+                    </h2>
+                        <hr>
+                        <p>
+                            Proin at vulputate risus. Duis volutpat scelerisque euismod. Sed rutrum interdum egestas. Curabitur tortor enim, ultrices id venenatis eu, laoreet vitae nibh. Maecenas eu tristique arcu. Duis in hendrerit orci.<br><br>
+                        </p>
+                        <h2>
+                      Entrena
+                    </h2>
+                        <hr>
+                        <p>
+                            Curabitur tortor enim, ultrices id venenatis eu, laoreet vitae nibh. Maecenas eu tristique arcu. Duis in hendrerit orci.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="scrollDown2">
+                <a href="#scrollDown2">SCROLL</a>
+            </div>
+        </section>
+        <!--section3 -->
+        <section id="scrollDown2" class="section3">
+            <div class="container_section3">
+                <div class="section3_a">
+                    <div class="section3_a_text">
+                        <h2>
+                        Conecta
+                      </h2>
+                        <hr>
+                        <p>
+                            Proin at vulputate risus. Duis volutpat scelerisque euismod. Sed rutrum interdum egestas. Curabitur tortor enim, ultrices id venenatis eu, laoreet vitae nibh. Maecenas eu tristique arcu. Duis in hendrerit orci.<br><br>
+                        </p>
+                        <h2>
+                        Comparti
+                      </h2>
+                        <hr>
+                        <p>
+                            Curabitur tortor enim, ultrices id venenatis eu, laoreet vitae nibh. Maecenas eu tristique arcu. Duis in hendrerit orci.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="scrollDown3">
+                <a href="#section1">SCROLL</a>
+            </div>
+        </section>
+        <!--------------footer-------------------->
+        <footer class="footer">
+            <div class="footer_container">
+                <p>
+                    OUTHLETES © - 2016
+                </p>
+
+                <a href="faqs" class="button">FAQS</a>
+
+            </div>
+        </footer>
+</body>
+
+</html>
